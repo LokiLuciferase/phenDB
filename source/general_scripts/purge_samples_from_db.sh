48bf2f356d0fc23b418e1a36a5b5a24caf0ef5d3
LokiLuciferase <luxxus1@gmail.com>
2017-11-23 00:04:10 +0100

    corrected line endings

--- source/general_scripts/purge_samples_from_db.sh
+++ source/general_scripts/purge_samples_from_db.sh
@@ -2,7 +2,7 @@
 
 cd /apps/phenDB/source/web_server/phenotypePredictionApp
 
-sqlite3 phenDB.sqlite3 "delete from phenotypePredictionApp_bin;"
+sqlite3 phenDB.sqlite3 "delete from phenotypePredictionApp_UploadedFile;"
 sqlite3 phenDB.sqlite3 "delete from phenotypePredictionApp_result_model;"
 sqlite3 phenDB.sqlite3 "delete from phenotypePredictionApp_result_enog;"
 sqlite3 phenDB.sqlite3 "VACUUM;"
