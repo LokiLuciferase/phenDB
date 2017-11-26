3732d0d0395e69b1198925c4de97352e50645818
peterpeneder <peterpeneder@gmx.at>
2017-11-26 19:53:50 +0100

    Merge remote-tracking branch 'origin/PP_devel_webserver' into PP_devel_webserver
    # Conflicts:
    #	source/web_server/businessLogic/startProcess.py

--- source/web_server/businessLogic/startProcess.py
+++ source/web_server/businessLogic/startProcess.py
@@ -13,7 +13,7 @@ class startProcessThread(threading.Thread):
     def run(self):
         print('startProcess called')
         #web_server_folder = "/apps/phenDB/source/web_server"
-        web_server_folder = "/apps/PhenDB_PP_devel/phenDB/source/web_server"
+        web_server_folder = "/apps/phenDB_devel_PP/phenDB/source/web_server"
         relFilePath = os.path.dirname(UploadedFile.objects.get(key=self.keyname).fileInput.url)
         infolder = os.path.join(str(web_server_folder), str(relFilePath)[1:])
         print("infolder:", infolder)
@@ -21,9 +21,9 @@ class startProcessThread(threading.Thread):
         # runscript_path = "/apps/phenDB/source/pipeline/run_picaPipeline.sh"
         # pipeline_path = "/apps/phenDB/source/pipeline/picaPipeline.nf"
         # above_workfolder = "/apps/phenDB/source/web_server/results/resultFiles"
-        runscript_path = "/apps/PhenDB_PP_devel/phenDB/source/pipeline/run_picaPipeline.sh"
-        pipeline_path = "/apps/PhenDB_PP_devel/phenDB/source/pipeline/picaPipeline.nf"
-        above_workfolder = "/apps/PhenDB_PP_devel/phenDB/source/web_server/results/resultFiles"
+        runscript_path = "/apps/phenDB_devel_PP/phenDB/source/pipeline/run_picaPipeline.sh"
+        pipeline_path = "/apps/phenDB_devel_PP/phenDB/source/pipeline/picaPipeline.nf"
+        above_workfolder = "/apps/phenDB_devel_PP/phenDB/source/web_server/results/resultFiles"
 
         
         pica_cutoff = "0.5"
@@ -31,7 +31,7 @@ class startProcessThread(threading.Thread):
 
         os.environ["DJANGO_SETTINGS_MODULE"] = "phenotypePrediction.settings"
         #os.environ["PYTHONPATH"] = "/apps/phenDB/source/web_server:$PYTHONPATH"
-        os.environ["PYTHONPATH"] = "/apps/PhenDB_PP_devel/phenDB/source/web_server:$PYTHONPATH"
+        os.environ["PYTHONPATH"] = "/apps/phenDB_devel_PP/phenDB/source/web_server:$PYTHONPATH"
 
         # create workfolder
         outfolder = os.path.join(above_workfolder, "{jn}_results".format(jn=self.keyname))
