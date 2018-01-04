0ac18c26ecd242bf7ba2d59a66b2394a34201ca3
peterpeneder <peterpeneder@gmx.at>
2018-01-04 20:56:35 +0100

    bugfixes

--- source/general_scripts/add_enogs_to_db.py
+++ source/general_scripts/add_enogs_to_db.py
@@ -11,6 +11,7 @@ ENOG_files = ["/var/www/eggnog/eggnog_4.5/data/bacNOG/bacNOG.annotations.tsv.gz"
               "/var/www/eggnog/eggnog_4.5/data/bactNOG/bactNOG.annotations.tsv.gz" ]
 
 enoglist=[]
+namlist=[]
 for annot in ENOG_files:
     print("Processing ", annot)
 #Open the annotations file, every line is an enog. Translate the Lettercode in the annotations-file using the
@@ -20,6 +21,7 @@ for annot in ENOG_files:
         for line in f:
             line=line.split("\t")
             enoglist.append(enog(enog_name=line[1], enog_descr=line[5].rstrip() ))
+            namelist.append(line[1])
             counter+=1
             sys.stdout.write('\r')
             sys.stdout.write("Adding enog nr. ")
@@ -27,4 +29,6 @@ for annot in ENOG_files:
             sys.stdout.flush()
 
 print("\n writing enogs to db...")
+
+print(len(namelist) != len(set(namelist)))
 enog.objects.bulk_create(enoglist)
--- source/general_scripts/run_add_enogs_to_db.sh
+++ source/general_scripts/run_add_enogs_to_db.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 export DJANGO_SETTINGS_MODULE="phenotypePrediction.settings"
-export PYTHONPATH="/apps/phenDB_devel_LL/source/web_server:$PYTHONPATH"
-source activate py3env
-cd /apps/phenDB_devel_LL/source/general_scripts
+export PYTHONPATH="/apps/phenDB_devel_PP/phenDB/source/web_server:$PYTHONPATH"
 
+#export PYTHONPATH="/apps/phenDB_devel_LL/source/web_server:$PYTHONPATH"
+#source activate py3env
+#cd /apps/phenDB_devel_LL/source/general_scripts
+cd /apps/phenDB_devel_PP/phenDB/source/general_scripts
 python3 add_enogs_to_db.py
