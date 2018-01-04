7337da0eac2d8fb9141205bce5b429504ead291e
peterpeneder <peterpeneder@gmx.at>
2018-01-04 21:13:23 +0100

    bugfixes

--- source/general_scripts/add_enogs_to_db.py
+++ source/general_scripts/add_enogs_to_db.py
@@ -11,7 +11,6 @@ ENOG_files = ["/var/www/eggnog/eggnog_4.5/data/bacNOG/bacNOG.annotations.tsv.gz"
               "/var/www/eggnog/eggnog_4.5/data/bactNOG/bactNOG.annotations.tsv.gz" ]
 
 enoglist=[]
-namlist=[]
 for annot in ENOG_files:
     print("Processing ", annot)
 #Open the annotations file, every line is an enog. Translate the Lettercode in the annotations-file using the
@@ -21,7 +20,6 @@ for annot in ENOG_files:
         for line in f:
             line=line.split("\t")
             enoglist.append(enog(enog_name=line[1], enog_descr=line[5].rstrip() ))
-            namelist.append(line[1])
             counter+=1
             sys.stdout.write('\r')
             sys.stdout.write("Adding enog nr. ")
