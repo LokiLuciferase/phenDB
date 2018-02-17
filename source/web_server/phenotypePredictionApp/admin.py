6cadcf7a38caa9c6a52d97530bbbbc5dabbdadf2
LokiLuciferase <luxxus1@gmail.com>
2018-02-17 21:52:13 +0100

    better formatting for admin pages

--- source/web_server/phenotypePredictionApp/admin.py
+++ source/web_server/phenotypePredictionApp/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+import os
 
 # Register your models here.
 
@@ -13,8 +14,20 @@ class JobDisplay(admin.ModelAdmin):
         else:
             return not obj.errors
 
-    no_errors.boolean = True
+    def link_to_results(obj):
+        if obj.fileOutput:
+            return "phen.csb.univie.ac.at/phendb/results/{k}".format(k=obj.key)
+        return obj.fileOutput
 
-    list_display = ('job_date', 'user_email', 'user_ip', 'finished_bins', 'total_bins', no_errors, 'fileInput', 'fileOutput')
+    def uploaded_file_name(obj):
+        if obj.fileInput:
+            return os.path.basename(obj.fileInput)
+        return obj.fileInput
+
+    no_errors.boolean = True
+    link_to_results.allow_tags = True
+
+    list_display = ('job_date', 'user_email', 'user_ip', 'finished_bins', 'total_bins', no_errors, uploaded_file_name, link_to_results)
+    list_display_links = None
 
 admin.site.register(UploadedFile, JobDisplay)
