95e98adc1bb6731e375c9bbca43be93c15f63dc2
Lukas Lüftinger <lukas.lueftinger@univie.ac.at>
2018-05-09 16:42:18 +0200

    begin framework for addition of precalculated genomes plus taxids

--- source/web_server/businessLogic/precalc_bins.py
+++ source/web_server/businessLogic/precalc_bins.py
@@ -3,11 +3,27 @@
 # Created by Lukas Lüftinger on 08/05/2018.
 #
 import os
+from time import sleep
 
 from phenotypePredictionApp.variables import *
 from enqueue_job import phenDB_enqueue
 from redis import Redis
 from rq import Queue
+
+def add_taxids_to_precalc_bins(mapfile):
+    import django
+    django.setup()
+    from phenotypePredictionApp.models import bin, UploadedFile
+
+    names_to_taxids = {}
+    with open(mapfile, "r") as mf:
+        for line in mf:
+            name, taxid = line.strip().split("\t")
+            names_to_taxids[name] = taxid
+
+    precalc_bins = bin.objects.filter(UploadedFile.key="PHENDB_PRECALC")
+    # TODO: add to each bin its corresponding taxid, then commit in batch
+
 
 
 ppath = PHENDB_BASEDIR + "/source/web_server:$PYTHONPATH"
@@ -27,9 +43,15 @@ os.makedirs(logfolder, exist_ok=True)
 print("Submitting precalculation job. Bins in folder {inf} will be added to the database.".format(inf=infolder))
 
 q = Queue(PHENDB_QUEUE, connection=Redis())
-pipeline_errorcode = q.enqueue_call(func=phenDB_enqueue,
+pipeline_call = q.enqueue_call(func=phenDB_enqueue,
                                     args=(ppath, pipeline_path, infolder, outfolder, 0.5, ""),
                                     timeout='72h',
                                     ttl='72h',
                                     job_id="PHENDB_PRECALC"
                                     )
+while pipeline_call.result == None:
+    sleep(10)
+
+if pipeline_call.result is True:
+    print("Precalculation was successful.")
+    # TODO: begin adding the taxids now
