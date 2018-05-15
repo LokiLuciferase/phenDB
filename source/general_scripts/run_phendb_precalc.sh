cd0dff23b03829d1d42c9d9b329f9fa40e83b627
Lukas Lüftinger <lukas.lueftinger@univie.ac.at>
2018-05-15 14:38:05 +0200

    set -e for all shell scripts which source variables

--- source/general_scripts/run_phendb_precalc.sh
+++ source/general_scripts/run_phendb_precalc.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -e
 source variables.sh
 
 export DJANGO_SETTINGS_MODULE="phenotypePrediction.settings"
--- source/general_scripts/run_redis.sh
+++ source/general_scripts/run_redis.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+set -e
 source variables.sh
 
 mkdir -p ${BASEDIR}/logs
