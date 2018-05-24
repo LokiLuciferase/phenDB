a2be24d2d76a7155e6a6929993908a41045e3145
florian <not@yourbusiness.com>
2018-05-24 17:01:03 +0200

    jsonify in template

--- source/web_server/phenotypePredictionApp/templatetags/jsonify.py
+++ source/web_server/phenotypePredictionApp/templatetags/jsonify.py
@@ -7,7 +7,7 @@ from django.template import Library
 
 register = Library()
 
-def jsonify(value):
+def jsonify(object):
     if isinstance(object, QuerySet):
         return serialize('json', object)
     return json.dumps(object)
