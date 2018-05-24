c89ba786662d2079198220f328bf639333872dbd
florian <not@yourbusiness.com>
2018-05-24 15:20:25 +0200

    testing: print all bins of request

--- source/web_server/phenotypePredictionApp/views.py
+++ source/web_server/phenotypePredictionApp/views.py
@@ -188,7 +188,7 @@ def getResults(request):
     except:
         print(traceback.format_exc())
 
-    print("len resultsDic " + len(resultsDic))
+    print("len resultsDic " + str(len(resultsDic)))
     context = {'result' : 'download/',
                'showResultCSS' : showResultCSS,
                'showNotification' : True if numAccessed == 1 else False,
