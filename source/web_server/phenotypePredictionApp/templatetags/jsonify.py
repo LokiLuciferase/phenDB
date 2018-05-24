943705e780256d6809117738995d7b13f8ca29cb
florian <not@yourbusiness.com>
2018-05-24 18:15:16 +0200

    jsonify in template

--- source/web_server/phenotypePredictionApp/templatetags/jsonify.py
+++ source/web_server/phenotypePredictionApp/templatetags/jsonify.py
@@ -1,18 +1,9 @@
-#code from https://djangosnippets.org/snippets/201/
 
 from django.core.serializers import serialize
-from django.db.models.query import QuerySet
-import json
 from django.template import Library
 
 register = Library()
 
 @register.filter(is_safe=True)
 def jsonify(object):
-    print("filter type object: " + str(type(object)))
-    print("filter size object: " + str(len(object)))
-    if isinstance(object, QuerySet):
-        #for singleObj in object:
-            #singleObj.bin.bin_name
-        return serialize('json', object)
     return serialize('json', object, use_natural_foreign_keys=True)
