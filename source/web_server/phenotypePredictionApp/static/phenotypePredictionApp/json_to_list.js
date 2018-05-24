67f1f3e208c58f36d73ce5e781f2df7653f3a754
florian <not@yourbusiness.com>
2018-05-24 18:41:53 +0200

    json_to_list for template

--- source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/json_to_list.js
+++ source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/json_to_list.js
@@ -1,8 +1,10 @@
+example_entry;
 
 function json_to_list(json_obj) {
     var result_arr = [];
     for (single_entry in json_obj) {
-        var sub_arr = Object.values(single_entry.fields)
+        example_entry = single_entry;
+        var sub_arr = Object.values(single_entry.fields);
         result_arr.push(sub_arr);
     }
     return result_arr;
