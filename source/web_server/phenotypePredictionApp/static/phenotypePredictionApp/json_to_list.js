b4f20fe85439c6972d4798f8a1eaa186f5a532df
florian <not@yourbusiness.com>
2018-05-24 19:41:14 +0200

    json_to_list for template

--- source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/json_to_list.js
+++ source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/json_to_list.js
@@ -1,10 +1,18 @@
 
-function json_to_list(json_obj) {
-    var result_arr = [];
-    var result_keys = Object.keys(json_obj[0].fields);
+function json_to_datatable_input(json_obj) {
+    var all_data = [];
+    var titles = []
+
+    var all_keys = Object.keys(json_obj[0].fields);
+    for(key in all_keys) {
+        let dicTmp = {title : key};
+        titles.push(dicTmp);
+    }
+
     for (i=0; i<json_obj.length; i++) {
         var sub_arr = Object.values(json_obj[i].fields);
         result_arr.push(sub_arr);
     }
-    return [result_keys, result_arr];
+
+    return [titles, all_data];
 }
