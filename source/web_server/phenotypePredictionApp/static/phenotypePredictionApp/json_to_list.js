a88bfb0848df62cd532bb2926fa5e24dda51f2c8
florian <not@yourbusiness.com>
2018-05-25 13:56:31 +0200

    DataTable: add filter (work in progress) / models as json

--- source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/json_to_list.js
+++ source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/json_to_list.js
@@ -4,12 +4,12 @@ function resultList_to_datatable_input(json_obj) {
     var titles = [];
 
     var all_keys = Object.keys(json_obj[0].fields);
-    for(i=0; i<all_keys.length; i++) {
+    for(let i=0; i<all_keys.length; i++) {
         let dicTmp = {title : all_keys[i]};
         titles.push(dicTmp);
     }
 
-    for (i=0; i<json_obj.length; i++) {
+    for (let i=0; i<json_obj.length; i++) {
         var sub_arr = Object.values(json_obj[i].fields);
         all_data.push(sub_arr);
     }
@@ -17,3 +17,18 @@ function resultList_to_datatable_input(json_obj) {
     return [titles, all_data];
 }
 
+//WARNING: This code could break if the Django model is changed
+function all_models_to_list(json_obj) {
+    var model_names = [];
+    var types = [];
+    var model_descriptions = [];
+    var model_train_dates = [];
+    for(let i=0; i<json_obj.length; i++) {
+        var row = Object.values(json_obj[i].fields);
+        model_names.push(row[0]);
+        types.push(row[1]);
+        model_descriptions.push(row[2]);
+        model_train_dates.push(row[3]);
+    }
+    return [model_names, types, model_descriptions, model_train_dates];
+}
