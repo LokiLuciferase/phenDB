f41db65247fff74e5afb2e2baca47ede305fc2d4
florian <not@yourbusiness.com>
2018-06-14 16:43:58 +0200

    resultslist_to_dt2_matrix function

--- source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/ui_logic.js
+++ source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/ui_logic.js
@@ -68,8 +68,30 @@ function getAllBins(resultsListJSValues) {
     });
     return unique_bins;
 }
-/*
-function resultslist_to_dt2_matrix(resultsListJSValues) {
-    var bins_arr = resultsListJSValues[0];
-    var models =
-} */
+
+function resultslist_to_dt2_matrix(resultsListJSValues, models) {
+    resultsListJSValues.sort(function (a, b) {
+        if (a[0] < b[0]) return -1;
+        else if (a[0] > b[0]) return 1;
+        else return 0;
+    });
+
+    var dt2_matrix;
+    var bin_prev = resultsListJSValues[0][0];
+    var row_index = 0;
+    var row = [bin_prev];
+    for(var i=0; i<resultsListJSValues.length; i++) {
+        var bin_tmp = resultsListJSValues[i][0];
+        var verdict = resultsListJSValues[i][2];
+        var model = resultsListJSValues[i][3];
+        var model_index = models.indexOf(model);
+        if(bin_prev != bin_tmp) {
+            dt2_matrix[row_index] = row;
+            row = [bin_tmp];
+            ++row_index;
+        }
+        row[model_index+1] = verdict;
+        bin_prev = bin_tmp;
+    }
+
+}
