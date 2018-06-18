0437a9a73d26469932469f0e0ae0bf804d2d937c
florian <not@yourbusiness.com>
2018-06-18 13:53:02 +0200

    fixed column / enable scroll

--- source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/result_figures.js
+++ source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/result_figures.js
@@ -18,7 +18,6 @@
             data: resultsListJSValues,
             columns: resultsListJSTitles,
             searching: true,
-            "scrollX": true,
             'dom' : '<ltp>', //controls which parts of the datatable should be rendered and in which order (e.g. paging control)
         } );
         return dataTable;
