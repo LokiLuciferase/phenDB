c633cc366a4ddc9abd67749903ae90beed1b380b
Lukas Lüftinger <luxxus1@gmail.com>
2018-06-19 11:20:58 +0200

    fix bug in taxonomy add script; update trait_dependencies file.

--- source/general_scripts/add_taxonomy_to_db.py
+++ source/general_scripts/add_taxonomy_to_db.py
@@ -2,6 +2,6 @@
 # Created by Lukas Lüftinger on 6/15/18.
 #
 from phenotypePredictionApp.variables import PHENDB_QUEUE, PHENDB_BASEDIR
-from enqueue_job import update_taxonomy
+from businessLogic.enqueue_job import update_taxonomy
 
 update_taxonomy(PHENDB_BASEDIR)
