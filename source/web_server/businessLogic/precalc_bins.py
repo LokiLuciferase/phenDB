4d0b9450a1b97e08d759d8634d95f1d3a670ca3c
Lukas Lüftinger <luxxus1@gmail.com>
2018-06-19 17:56:20 +0200

    getting scientific name from taxon table upon precalculation

--- source/web_server/businessLogic/precalc_bins.py
+++ source/web_server/businessLogic/precalc_bins.py
@@ -31,7 +31,7 @@ Entrez.email = "test@test.com"
 def get_latest_refseq_genomes(n_days, only_reference=False, max_n=None, latest=None):
     records = []
     latest_date = date.today() if latest is None else datetime.strptime(latest, "%Y/%m/%d").date()
-    minus_n_days = latest_date - timedelta(days=n_days)
+    minus_n_days = latest_date - timedelta(days=int(n_days))
     dateformatted = minus_n_days.strftime("%Y/%m/%d")
     latestformatted = latest_date.strftime("%Y/%m/%d")
     representative_str = "" if only_reference else "OR \"representative genome\"[filter]"
@@ -58,7 +58,9 @@ def get_latest_refseq_genomes(n_days, only_reference=False, max_n=None, latest=N
             print(e)
             continue
     if max_n is not None:
-        return records[:max_n]
+        records = records[:max_n]
+    for entry in records:
+        print("Found RefSeq entry: {e}".format(e=entry[0]))
     return records
 
 
@@ -91,13 +93,17 @@ def check_add_precalc_job():
 
 
 def add_taxids_to_precalc_bins(los):
-    from phenotypePredictionApp.models import Bin, Job
+    from phenotypePredictionApp.models import Bin, Job, Taxon
     for name, taxid, assembly_id, ftppath in los:
         binname = "PHENDB_PRECALC_" + assembly_id + ".fna.gz"
         givenbin = Bin.objects.filter(bin_name=binname)
+        given_taxon = Taxon.objects.filter(tax_id=taxid)[0]
         if not givenbin:
             raise RuntimeError("Bin {gb} not found in database.".format(gb=binname))
-        givenbin.update(tax_id=str(taxid), assembly_id=str(assembly_id), taxon_name="", taxon_rank="")
+        givenbin.update(tax_id=str(taxid),
+                        assembly_id=str(assembly_id),
+                        taxon_name=str(given_taxon.taxon_name),
+                        taxon_rank=str(given_taxon.taxon_rank))
 
 
 def main():
@@ -131,13 +137,14 @@ def main():
                                    )
     while pipeline_call.result is None:
         sleep(10)
-        print(pipeline_call.result)
 
     if pipeline_call.result is 0:
         print("Precalculation was successful.")
         print("Adding taxonomic information to precalculated bins.")
         add_taxids_to_precalc_bins(gtlist)
         print("Finished. added {lolos} items to database.".format(lolos=len(gtlist)))
+    else:
+        print("Precalculation has failed!")
 
 if __name__ == "__main__":
     main()
