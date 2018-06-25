86765a0749d3cd6d684748bee1c5287ba9769cba
florian <not@yourbusiness.com>
2018-06-25 16:21:50 +0200

    refactoring ui logic / debugging

--- source/web_server/phenotypePredictionApp/ui/ResultFigures.py
+++ source/web_server/phenotypePredictionApp/ui/ResultFigures.py
@@ -1,20 +1,18 @@
 import json
 from phenotypePredictionApp.models import Job, PicaResult, BinInJob, PicaModel
+from django.core.serializers import serialize
 
 class ResultFigures:
     def initialize(job):
         all_bins = BinInJob.objects.filter(job=job)
         db_data = list(map(lambda x: PicaResult.objects.filter(bin=x.bin), all_bins))
-        print(db_data)
         resultFigures = ResultFigures()
         #resultFigures.__buildTraitPredictionTable(db_data)
         return resultFigures
 
     def __buildTraitPredictionTable(self, db_data):
         #TODO: change
-        json_data = json.dumps(db_data)
-        self.traitPredictionTable = json_data
+        self.traitPredictionTable = serialize('json', db_data, use_natural_foreign_keys=True)
 
     def getTraitPredictionTable(self):
-        #return self.traitPredictionTable
-        return ""
+        return self.traitPredictionTable
