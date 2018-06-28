0a9461fc8da8bfae6b2bd572cee14eff91759b38
florian <not@yourbusiness.com>
2018-06-28 21:41:42 +0200

    refactor due to db changes

--- source/web_server/phenotypePredictionApp/serialization/serialization.py
+++ source/web_server/phenotypePredictionApp/serialization/serialization.py
@@ -34,16 +34,18 @@ class TableCalcTraitPrediction:
         for bin_obj in self.picaResultForUI.all_bins:
             single_pica_result = PicaResult.objects.filter(bin=bin_obj.bin)
             bin_name = BinInJob.objects.get(bin=bin_obj.bin, job=self.picaResultForUI.job)
-            arr.append(self.__parse_bin(single_pica_result, bin_name))
+            arr = arr + self.__parse_bin(single_pica_result, bin_name)
         return arr
 
 
     def __parse_bin(self, single_pica_result, bin_name):
         arr = []
         for item in single_pica_result:
-            arr.append("" if bin_name.bin_alias is None else bin_name.bin_alias)
-            arr.append(item.model.model_name)
-            arr.append("+" if item.verdict else "-")
-            arr.append(item.pica_pval)
-            arr.append(item.accuracy)
+            single_row = []
+            single_row.append("" if bin_name.bin_alias is None else bin_name.bin_alias)
+            single_row.append(item.model.model_name)
+            single_row.append("+" if item.verdict else "-")
+            single_row.append(item.pica_pval)
+            single_row.append(item.accuracy)
+            arr.append(single_row)
         return arr
