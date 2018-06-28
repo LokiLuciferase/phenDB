a12b3b5ab43b3b684db524be3e92258b26f660b2
florian <not@yourbusiness.com>
2018-06-28 21:12:07 +0200

    refactor due to db changes

--- source/web_server/phenotypePredictionApp/serialization/serialization.py
+++ source/web_server/phenotypePredictionApp/serialization/serialization.py
@@ -10,11 +10,8 @@ class PicaResultForUI:
         return self.__table_calc_trait_prediction()
 
     def __table_calc_trait_prediction(self):
-        for bin_obj in self.all_bins:
-            single_pica_result = PicaResult.objects.filter(bin=bin_obj.bin)
-            bin_name = BinInJob.objects.filter(bin=bin_obj.bin, job=self.job)
-            test = zip(single_pica_result, bin_name)
-            return test
+        obj = TableCalcTraitPrediction(self)
+        return obj.calc()
 
 
     def __table_calc_prediction_details(self):
@@ -23,10 +20,29 @@ class PicaResultForUI:
     def __table_calc_trait_counts(self):
         pass
 
-    '''def __calc
 
-def picaResultForUI(job):
-    resultList = []
-    all_bins = BinInJob.objects.filter(job=job)
-    for bin_obj in all_bins:
-        resultsList += PicaResult.objects.filter(bin=bin_obj.bin).select_related('bin') '''
+class TableCalcTraitPrediction:
+
+    titles = {'bin' : 'Bin'}
+
+    def __init__(self, picaResultForUI):
+        self.picaResultForUI = picaResultForUI
+
+
+    def calc(self):
+        arr = []
+        for bin_obj in self.picaResultForUI.all_bins:
+            single_pica_result = PicaResult.objects.filter(bin=bin_obj.bin)
+            bin_name = BinInJob.objects.filter(bin=bin_obj.bin, job=self.picaResultForUI.job)
+            arr += self.__parse_bin(single_pica_result, bin_name)
+        return arr;
+
+
+    def __parse_bin(self, single_pica_result, bin_name):
+        arr = []
+        for item in single_pica_result:
+            arr += bin_name.bin_alias
+            arr += item.model.model_name
+            arr += "+" if item.verdict else "-"
+            arr += item.pica_pval
+            arr += item.accuracy
