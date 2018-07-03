6201f353670a340c522157d1a0c3e36a91b022b7
florian <not@yourbusiness.com>
2018-07-03 15:58:26 +0200

    debugging

--- source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/result_figures.js
+++ source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/result_figures.js
@@ -61,7 +61,7 @@ function DataTable(data, titles, identifier) {
     }
 
     this.add_colvis_filter = function(button_text) {
-        var colvisOptions = {extend: "colvis", collectionLayout: "fixed four-column", text: button_text, postfixButtons: [{text: "Test"}]}
+        var colvisOptions = {extend: "colvis", collectionLayout: "fixed four-column", text: button_text, postfixButtons: [{extend: 'columnToggle', text: "Hide all", visibility: false}]}
         this.dataTable.button().add(0, colvisOptions);
 
     }
