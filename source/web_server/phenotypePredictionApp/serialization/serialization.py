e0c151ad668d0442d49e722e3c68a3b13d21db72
Lukas Lüftinger <luxxus1@gmail.com>
2018-07-04 13:53:11 +0200

    much faster serialization

--- source/web_server/phenotypePredictionApp/serialization/serialization.py
+++ source/web_server/phenotypePredictionApp/serialization/serialization.py
@@ -10,6 +10,8 @@ class PicaResultForUI:
         self.disable_cutoffs = bool(job.disable_cutoffs) if not disable_cutoffs else bool(disable_cutoffs)
         self.all_bins_in_job = BinInJob.objects.filter(job=job).select_related('bin')
         self.all_bins = [x.bin for x in self.all_bins_in_job]
+        self.bin_alias_list = [x.bin_alias for x in self.all_bins_in_job]
+        print("fetched all bins")
         all_models_for_currentjob = [x.model for x in PicaResult.objects.filter(bin=self.all_bins[0]).select_related('model')]
         all_model_names_for_cj = set()
         self.newest_models_for_currentjob = []
@@ -23,11 +25,36 @@ class PicaResultForUI:
                 all_model_names_for_cj.add(pmn)
                 self.newest_models_for_currentjob.append(pm)
         self.all_results_for_currentjob = PicaResult.objects.filter(bin__in=self.all_bins, model__in=self.newest_models_for_currentjob)
-        self.bin_alias_list = [x.bin_alias for x in self.all_bins_in_job]
+        print("Done getting all results.")
+        self.resdic = self.__make_results_dict()
+        print("Done making results dict")
         self.__calc_prediction_details()
+        print("done getting details")
         self.__calc_prediction()
+        print("done getting preds")
         self.__calc_trait_counts()
+        print("done getting trait counts")
         self.__calc_bin_summary()
+        print("done getting summary")
+
+    def __make_results_dict(self):
+        resdic = {}
+        bin_id_to_bij_id = {x.bin_id: x.bin_alias for x in self.all_bins_in_job}
+        bin_id_to_alias = {x.id: bin_id_to_bij_id[x.id] for x in self.all_bins}
+        model_id_to_name = {x.id: x.model_name for x in self.newest_models_for_currentjob}
+        for res in list(self.all_results_for_currentjob.values()):
+            bin_alias = bin_id_to_alias[res.get("bin_id")]
+            model_name = model_id_to_name[res.get("model_id")]
+            verdict = res.get("verdict")
+            mba = res.get("accuracy")
+            pconf = res.get("pica_pval")
+            nc_masked = res.get("nc_masked")
+            bin_in_resdic = resdic.setdefault(bin_alias, {})
+            bin_in_resdic[model_name] = {"verdict": verdict,
+                                         "accuracy": mba,
+                                         "pica_pval": pconf,
+                                         "nc_masked": nc_masked}
+        return resdic
 
     def __calc_prediction_details(self):
         self.prediction_details = _PredictionDetails(self)
@@ -41,10 +68,10 @@ class PicaResultForUI:
     def __calc_bin_summary(self):
         self.bin_summary = _BinSummary(self)
 
-    def _apply_masks(self, result):
-        result_string = "+" if result.verdict else "-"
-        nc_masked = result.nc_masked
-        nd_masked = result.pica_pval <= self.requested_conf or result.accuracy <= self.requested_balac
+    def _apply_masks(self, resultdic):
+        result_string = "+" if resultdic["verdict"] else "-"
+        nc_masked = resultdic["nc_masked"]
+        nd_masked = resultdic["pica_pval"] <= self.requested_conf or resultdic["accuracy"] <= self.requested_balac
 
         if self.disable_cutoffs or (not nd_masked and not nc_masked):
             return result_string
@@ -71,22 +98,22 @@ class _PredictionDetails:
 
     def __calc(self):
         arr = []
-        for bij in self.picaResultForUI.all_bins_in_job:
-            single_pica_result = self.picaResultForUI.all_results_for_currentjob.filter(bin=bij.bin)
-            bin_name = bij.bin_alias
-            arr += self.__parse_bin(single_pica_result, bin_name)
+        rd = self.picaResultForUI.resdic
+        for bin_name in rd.keys():
+            bin_dic = rd[bin_name]
+            arr += self.__parse_bin(bin_dic, bin_name)
         self.values = arr
 
-
-    def __parse_bin(self, single_pica_result, bin_name):
+    def __parse_bin(self, bin_dic, bin_name):
         arr = []
-        for item in single_pica_result:
+        for model_name in sorted(bin_dic.keys()):
+            model_dic = bin_dic[model_name]
             single_row = []
             single_row.append(bin_name)
-            single_row.append(item.model.model_name)
-            single_row.append(self.picaResultForUI._apply_masks(item))
-            single_row.append(round(item.pica_pval, 2))
-            single_row.append(round(item.accuracy, 2))
+            single_row.append(model_name)
+            single_row.append(self.picaResultForUI._apply_masks(model_dic))
+            single_row.append(round(model_dic["pica_pval"], 2))
+            single_row.append(round(model_dic["accuracy"], 2))
             arr.append(single_row)
         return arr
 
@@ -98,17 +125,15 @@ class _Prediction:
 
     def __calc(self):
         self.values = []
-        for bin_in_job in self.picaResultForUI.all_bins_in_job:
-            bin_name = bin_in_job.bin_alias
-            bin = bin_in_job.bin
+        rd = self.picaResultForUI.resdic
+        for bin_name in rd.keys():
+            bin_dic = rd[bin_name]
             values_tmp = [bin_name]
             self.titles = [""]
-            for pica_model in self.picaResultForUI.newest_models_for_currentjob:
-                pica_result = self.picaResultForUI.all_results_for_currentjob.filter(bin=bin, model=pica_model)
-                if len(pica_result) == 0:
-                    continue # model not used in this prediction (e.g. old model)
-                self.titles.append({"title": pica_result[0].model.model_name})
-                values_tmp.append(self.picaResultForUI._apply_masks(pica_result[0]))
+            for model_name in sorted(bin_dic.keys()):
+                model_dic = rd[bin_name][model_name]
+                self.titles.append({"title": model_name})
+                values_tmp.append(self.picaResultForUI._apply_masks(model_dic))
             self.values.append(values_tmp)
 
     def get_values(self):
@@ -127,18 +152,22 @@ class _TraitCounts:
 
     def __calc(self):
         self.values = []
-        for pica_model in self.picaResultForUI.newest_models_for_currentjob:
-            print("pica_model " + pica_model.model_name)
-            pica_results = self.picaResultForUI.all_results_for_currentjob.filter(bin__in=self.picaResultForUI.all_bins, model=pica_model)
-            print(len(pica_results))
-            if(len(pica_results) == 0):
-                continue # model not used in this prediction (e.g. old model)
-            all_results = [self.picaResultForUI._apply_masks(x) for x in pica_results]
-            true_count = len([x for x in all_results if x == "+"])
-            false_count = len([x for x in all_results if x == "-"])
-            nd_count = len([x for x in all_results if x == "n.d."])
-            nc_count = len([x for x in all_results if x == "n.c."])
-            self.values.append([pica_model.model_name, true_count, false_count, nd_count, nc_count])
+        rd = self.picaResultForUI.resdic
+        model_names = [x.model_name for x in self.picaResultForUI.newest_models_for_currentjob]
+        countdic = {x: {"+":0, "-":0, "n.d.": 0, "n.c.":0} for x in model_names}
+
+        for bin_name in rd.keys():
+            bin_dic = rd[bin_name]
+            for model_name in bin_dic.keys():
+                model_dic = bin_dic[model_name]
+                countdic[model_name][self.picaResultForUI._apply_masks(model_dic)] += 1
+
+        for model_name in sorted(model_names):
+            plus = countdic[model_name]["+"]
+            minus = countdic[model_name]["-"]
+            nd = countdic[model_name]["n.d."]
+            nc = countdic[model_name]["n.c."]
+            self.values.append([model_name, plus, minus, nd, nc])
 
     def get_values(self):
         return self.values
