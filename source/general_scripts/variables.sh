032e6622ca330203f239e10b793a1ec07bb34afc
Lukas Lüftinger <luxxus1@gmail.com>
2018-07-05 10:02:06 +0200

    add from flo and switch to devel db

--- source/general_scripts/variables.sh
+++ source/general_scripts/variables.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 BASEDIR="/apps/phenDB_devel_LL"
-DB="phenDB"
+DB="phenDB_devel_LL"
 
 export PYTHONPATH="/apps/phenDB_devel_LL/source/web_server:$PYTHONPATH"
 export DJANGO_SETTINGS_MODULE="phenotypePrediction.settings"
