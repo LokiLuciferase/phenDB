d81b33682dee255be5bfb41aa1929cdeaf3a98cd
florian <not@yourbusiness.com>
2018-07-06 11:49:48 +0200

    try out fixed column

--- source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/result_figures.js
+++ source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/result_figures.js
@@ -12,6 +12,7 @@ function DataTable(data, titles, identifier) {
             searching: true,
             autoWidth: false,
             dom: '<"table_buttons"B>l<"result_table"t><"table_pagination"p>',
+            scrollX: true,
             fixedColumns:   {
                 leftColumns: 1,
             },
