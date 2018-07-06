2b50f8d6f50461a8aa5748c4a9ad727519896ac3
florian <not@yourbusiness.com>
2018-07-06 12:14:09 +0200

    datatable dom changes

--- source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/result_figures.js
+++ source/web_server/phenotypePredictionApp/static/phenotypePredictionApp/result_figures.js
@@ -6,7 +6,7 @@ function DataTable(data, titles, identifier) {
 
     this.initialize_data_table = function () {
         var dataTable = $(this.identifier).DataTable({
-            "lengthMenu": [[30,50, 100, -1], [50, 100, "All"]],
+            "lengthMenu": [[30,50, 100, -1], [30, 50, 100, "All"]],
             data: this.data,
             columns: this.titles,
             searching: true,
