3b56e865a624114f84e03f1db968a079349effeb
root <root@phen.csb.univie.ac.at>
2020-11-06 16:00:58 +0100

    fix permissions

--- source/general_scripts/variables.sh
+++ source/general_scripts/variables.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-export BASEDIR="/apps/phenDB"
-export PHENDB_QUEUE='phenDB'
-export DB="phenDB"
-export PHENDB_DEBUG=False
+export BASEDIR="/root/rebuild/phenDB"
+export PHENDB_QUEUE='phenDB_testing'
+export DB="phenDB_devel_LL"
+export PHENDB_DEBUG=True
 
 export PYTHONPATH="${BASEDIR}/source/web_server:$PYTHONPATH"
 export DJANGO_SETTINGS_MODULE="phenotypePrediction.settings"
