9050a1495015ca0f330e534e243ec96a2da62d34
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2020-11-06 16:44:00 +0100

    further parametrize basedir and PICA model dir in nextflow script

--- source/general_scripts/variables.sh
+++ source/general_scripts/variables.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-export BASEDIR="/root/rebuild/phenDB"
+export BASEDIR='/root/rebuild/phenDB'
 export PHENDB_QUEUE='phenDB_testing'
-export DB="phenDB_devel_LL"
+export PHENDB_MODEL_DIR='/apps/PICA/models_devel_LL'
+export DB='phenDB_devel_LL'
 export PHENDB_DEBUG=True
 
 export PYTHONPATH="${BASEDIR}/source/web_server:$PYTHONPATH"
