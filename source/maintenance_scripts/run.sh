299e757c292ca390b6fab051b7b8852a94a57cfd
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2020-12-04 20:11:29 +0100

    fix mod_wsgi setting

--- source/maintenance_scripts/run.sh
+++ source/maintenance_scripts/run.sh
@@ -2,7 +2,7 @@
 
 source source/maintenance_scripts/variables.sh
 [[ "$(mount | grep /apps/phenDB/data)" == "" && "${PHENDB_DEBUG}" != "True" ]] && echo "Please mount the data directory to /apps/phenDB/data." 1>&2 && return
-service mysql restart
+sudo service mysql restart || sudo service mariadb restart
 bash source/maintenance_scripts/phenDB.sh --load-db
 bash source/maintenance_scripts/phenDB.sh --run-fg
 bash source/maintenance_scripts/phenDB.sh --dump-db
