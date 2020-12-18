5be7fcf35f5fa16491826a933793e2b13efddccd
Lukas Lüftinger <lukas.lueftinger@ares-genetics.com>
2020-12-18 21:55:48 +0100

    fix environment variable parsing

--- source/web_server/phenotypePredictionApp/variables.py
+++ source/web_server/phenotypePredictionApp/variables.py
@@ -5,4 +5,4 @@ PHENDB_BASEDIR = os.environ["PHENDB_BASEDIR"]
 PHENDB_DATA_DIR = os.environ["PHENDB_DATA_DIR"]
 PHENDB_QUEUE = os.environ["PHENDB_QUEUE"]
 PHENDB_DB = os.environ["PHENDB_DB_NAME"]
-PHENDB_DEBUG = bool(os.environ["PHENDB_DEBUG"])
+PHENDB_DEBUG = os.environ["PHENDB_DEBUG"] == 'True'
